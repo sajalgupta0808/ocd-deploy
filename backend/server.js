8df7548e47a649d555b4a9615953e2d595c06057
sajalgupta0808 <agrasajalgupta@gmail.com>
2025-09-01 20:36:18 +0530

    added patch in cors

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.use(helmet());
 app.use(cors(
   {
   origin: "*",
-  methods: ["POST", "GET", "PUT", "DELETE"],
+  methods: ["POST", "GET", "PUT", "DELETE", "PATCH"],
   credentials: true
 }
 ));
